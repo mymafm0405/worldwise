febb6bb5d445301ccf8a6fa099408e2e9b85a3d6
Mahmoud Yhya <mymafm0405@gmail.com>
2023-08-08 11:39:30 +0300

    adding a backbutton comp and before adding the map

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css'
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,16 +11,17 @@ const formatDate = (date) =>
     weekday: "long",
   }).format(new Date(date));
 
-export default function CityItem({city}) {
-    const {cityName, emoji, date, id, position } = city;
+export default function CityItem({ city }) {
+  const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities()
   return (
     <li >
-        <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ''}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
-        </Link>
+      </Link>
     </li>
   )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
 import styles from "./Form.module.css";
+import BackButton from "./BackButton";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -53,10 +54,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type='primary'>ADD</Button>
-        <Button type='back' onClick={(e) => {
-          e.preventDefault()
-          navigate(-1)
-          }}>&larr; Back</Button>
+        <BackButton />
       </div>
     </form>
   );
